d16d8b02fe5e1f1887522dd9c53cec5d1a40cefb
Ivan Aytzhanov <aitvann@gmail.com>
2026-04-07 15:25:48 +0300

    feat(rust): 2339

--- Rust/closest-to-zero/src/main.rs
+++ Rust/closest-to-zero/src/main.rs
@@ -1,3 +1,22 @@
+//! [2239. Find Closest Number to Zero](https://leetcode.com/problems/find-closest-number-to-zero/description/)
+
+struct Solution;
+
+impl Solution {
+    pub fn find_closest_number(nums: Vec<i32>) -> i32 {
+        use std::cmp;
+        nums.into_iter()
+            .min_by(|&num1, &num2| match num1.abs().cmp(&num2.abs()) {
+                cmp::Ordering::Less => cmp::Ordering::Less,
+                cmp::Ordering::Equal => cmp::Reverse(num1).cmp(&cmp::Reverse(num2)),
+                cmp::Ordering::Greater => cmp::Ordering::Greater,
+            })
+            .unwrap()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![-4, -2, 1, 4, 8];
+    let res = Solution::find_closest_number(nums);
+    println!("{res}");
 }
